9112dfb17a588ae4e5b034f3b6463ec22e8744ac
James Pederson <james@jpederson.com>
2018-05-27 13:19:33 -0500

    Ad rotation with new ad.

---
+++ js/src/ad-rotate.js
@@ -0,0 +1,18 @@
+
+
+$(function(){
+
+	$(".ad > div:gt(0)").hide();
+
+	setInterval( function(){
+		$('.ad > div:first')
+			.hide()
+			.next()
+			.show()
+			.end()
+			.appendTo('.ad');
+		console.log('rotate');
+	}, 4000);
+	
+});
+
